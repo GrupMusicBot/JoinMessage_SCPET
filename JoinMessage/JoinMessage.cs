ee146ddfa8749358a9387e67bc4af1836f259953
GrupTheMusicBot "Kuebjii <bobwthomp@gmail.com>
2021-02-26 11:39:13 -0700

    Updated File Structure
    better file structure

---
+++ JoinMessage/JoinMessage.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using CensusAPI.Features;
+
+namespace SCPET_JoinMessage
+{
+    public class JoinMessage : Plugin<JoinConfig>
+    {
+        public override string Name => "JoinMessage";
+
+        public override Version Version => new Version(1, 0, 1);
+
+        public override string Author => "KuebV";
+
+        public static JoinMessage Instance { get; private set; }
+
+        public override void Disable()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Enable()
+        {
+            Instance = this;
+            CensusCore.CensusCore.InjectEvents();
+
+        }
+    }
+}
