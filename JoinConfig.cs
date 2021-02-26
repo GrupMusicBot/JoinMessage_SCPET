3ebd718e34461d0e2048c2ee5ced8e075e725bbd
GrupTheMusicBot "Kuebjii <42746052+GrupMusicBot@users.noreply.github.com>
2021-02-25 20:16:20 -0700

    Added Plugin

---
+++ JoinConfig.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CensusAPI.Interfaces;
+
+namespace SCPET_JoinMessage
+{
+    public class JoinConfig : IConfig
+    {
+        public bool IsEnabled { get; set; } = true;
+        public string BroadcastMessage { get; set; } = "Welcome to my server!";
+        public string ChatMessage { get; set; } = "Welcome %player%, to my Server!";
+
+        [Description("How long does the broadcasted message appear on the players screen")]
+        public int Timer { get; set; } = 5;
+    }
+}
